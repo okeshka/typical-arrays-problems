72e1bbf418ae74a3b9ffcdfa0049c1dac75264c7
okeshka <zakladniy@gmail.com>
2020-09-09 21:00:51 +0300

    resolve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((akk, element) => {akk += element; return akk}, 0) / array.length;
 }
