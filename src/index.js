dcec9a7d8c6dff2c7fa53fc3ecc16161ada89bdd
okeshka <zakladniy@gmail.com>
2020-09-09 21:35:01 +0300

    one more variant

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 exports.min = function min (array) {
-  if (!array || !array.length) return 0;
-  return Math.min(...array);
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (prev, current) => Math.min(prev, current) );
 }
 
 exports.max = function max (array) {
-  if (!array || !array.length) return 0;return 0;
-  return Math.max(...array);
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (prev, current) => Math.max(prev, current) );
 }
 
 exports.avg = function avg (array) {
-  if (!array || !array.length) return 0;
-  return array.reduce((akk, element) => {akk += element; return akk}, 0) / array.length;
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (sum, i) => sum + i ) / array.length;
 }
